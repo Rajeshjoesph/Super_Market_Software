ee4f893f2e280f4ccf3f3e161cf58f82de137479
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-09-10 18:00:28 +0530

    Create Stock Detail View page

--- src/OpenStock/OpenStock.jsx
+++ src/OpenStock/OpenStock.jsx
@@ -1,36 +1,69 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Paper from "@mui/material/Paper";
+import {
+  TableHeader,
+  TableRowContent,
+  VirtuosoTableComponents,
+} from "../Component/table";
+import { TableVirtuoso } from "react-virtuoso";
 
 const OpenstockDetail = () => {
-  const [openstock, setopenStock] = useState();
+  const [openStock, setopenStock] = useState([]);
 
   useEffect(() => {
     const stockApi = async () => {
-      await axios
-        .get("http://localhost:4000/openstock")
-        .then((res) => {
-          if (res.status === 200) {
-            setopenStock(res.data.data);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      try {
+        const res = await axios.get("http://localhost:4000/openstock");
+
+        if (res.status === 200) {
+          setopenStock(res.data.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
     };
     stockApi();
   }, []);
 
-  console.log(openstock);
+  console.log(openStock);
+  const columnWidth = 52;
+
+  const columns = [
+    { width: columnWidth, label: "ItemCode", dataKey: "itemCode" },
+    { width: columnWidth, label: "ItemName", dataKey: "itemName" },
+    { width: columnWidth, label: "Selling", dataKey: "sellingPrices" },
+    { width: columnWidth, label: "Mrp", dataKey: "mrpPrices" },
+    { width: columnWidth, label: "Stock", dataKey: "stockLevel" },
+
+    { width: columnWidth, label: "Encode", dataKey: "encode" },
+  ];
 
   return (
     <div>
       <h1>Open stock </h1>
+      <Paper style={{ height: 800, width: "100%" }} className="h-1/4 w-fill">
+        {openStock.length > 0 ? (
+          <TableVirtuoso
+            data={openStock}
+            components={VirtuosoTableComponents}
+            fixedHeaderContent={() => <TableHeader columns={columns} />}
+            itemContent={(index, row) => (
+              <TableRowContent columns={columns} row={row} />
+            )}
+          />
+        ) : (
+          <p> Loading Data...</p>
+        )}
+      </Paper>
+
+      {/*
       {openstock?.map((item) => (
         <>
           <p>{item.itemCode}</p>
           <p>{item.itemName}</p>
         </>
-      ))}
+      ))} */}
     </div>
   );
 };
--- src/Purchase/PurchaseHistory.jsx
+++ src/Purchase/PurchaseHistory.jsx
@@ -1,58 +1,63 @@
 import React, { useEffect, useState } from "react";
-import { RiEdit2Fill } from "react-icons/ri";
 import axios from "axios";
-import { CiMenuKebab } from "react-icons/ci";
+import Paper from "@mui/material/Paper";
+import {
+  TableHeader,
+  TableRowContent,
+  VirtuosoTableComponents,
+} from "../Component/table";
+import { TableVirtuoso } from "react-virtuoso";
+import { RiEdit2Fill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
 const PurchaseHistory = () => {
-  const [veiwDetail, setViewDetail] = useState();
+  const [veiwDetail, setViewDetail] = useState([]);
+
   useEffect(() => {
     const recivedata = async () => {
-      axios
-        .get("http://localhost:4000/purchaseentry")
-        .then((res) => {
-          if (res.status === 200) {
-            setViewDetail(res.data.data);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      try {
+        const res = await axios.get("http://localhost:4000/purchaseentry");
+
+        if (res.status === 200) {
+          setViewDetail(res.data.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
     };
     recivedata();
   }, []);
+  console.log(veiwDetail);
+  const columnWidth = 52;
+  const PurchaseColumn = [
+    { width: columnWidth, label: "Pur Date", dataKey: "entrydate" },
+    { width: columnWidth, label: "Supplyer Name", dataKey: "supplyerName" },
+    { width: columnWidth, label: "Invoice No", dataKey: "invoiceNo" },
+    { width: columnWidth, label: "Invoice Amt", dataKey: "billAmt" },
+    {
+      width: columnWidth,
+      label: "Edit",
+      dataKey: "edit",
+      link: `/purchaseentry/_id`,
+    },
+  ];
 
   return (
-    <div className="p-2">
-      <h1>Purchase History</h1>
-      <div className="w-full bg-gray-400 flex justify-between rounded-t-lg ">
-        <p className="w-56 bg-red-400">Purchase Date</p>
-        <p className="w-50 bg-red-400">supplyerName</p>
-        <p className="w-50 bg-red-400">invoiceNo</p>
-        <p className="w-50 bg-red-400">Bill Amount</p>
-        <p className="w-50 bg-red-400">Grn No</p>
-        <p className="w-50 bg-red-400">Edit</p>
-        <p className="w-50 bg-red-400">Menu</p>
-        {/* <h1>{e.enrtyDate}</h1> */}
-      </div>
-      {veiwDetail?.map((e) => (
-        <div className="w-full h-b bg-gray-400 my-4 px-5 flex justify-between rounded-md">
-          {/* <h1>hello</h1> */}
-          <h1>{e.enrtyDate}</h1>
-          <h1>{e.supplyerName}</h1>
-          <h1>{e.invoiceNo}</h1>
-          <h1>{e.billAmt}</h1>
-          <h1>{e._id}</h1>
-          <h1>
-            <Link to={`/purchaseentry/${e._id}`}>
-              <RiEdit2Fill />
-            </Link>
-          </h1>
-          <h1>
-            <CiMenuKebab />
-          </h1>
-        </div>
-      ))}
+    <div>
+      <Paper style={{ height: 800, width: "100%" }}>
+        {veiwDetail.length > 0 ? (
+          <TableVirtuoso
+            data={veiwDetail}
+            components={VirtuosoTableComponents}
+            fixedHeaderContent={() => <TableHeader columns={PurchaseColumn} />}
+            itemContent={(index, row) => (
+              <TableRowContent key={index} columns={PurchaseColumn} row={row} />
+            )}
+          />
+        ) : (
+          <p>LOading Data...</p>
+        )}
+      </Paper>
     </div>
   );
 };
