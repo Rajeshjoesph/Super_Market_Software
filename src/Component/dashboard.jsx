5f3b96f8850f06abbb32e02e2d13e2b0ee622de3
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-09-19 02:32:20 +0530

    modify dashbord code

--- src/Component/dashboard.jsx
+++ src/Component/dashboard.jsx
@@ -11,25 +11,8 @@ const Dashboard = () => {
     <div className="flex">
       <Navbar />
       <div className="App  items-center  justify-between h-screen w-full space-y-4 ">
-        <Link to={"/create"}>
-          <Button
-            label="Create Product"
-            styleType="primary"
-            className="gap-20"
-          />
-        </Link>
-        <Link to={"/Allproduct"}>
-          <Button label="View Product" styleType="secondary" />
-        </Link>
-        <Link to={"/PurchaseEntry"}>
-          <Button label="Prurchase Entry" styleType="secondary" />
-        </Link>
-        <Link to={"/PurchaseHistory"}>
-          <Button label="Prurchase History" styleType="secondary" />
-        </Link>
+        <h1>Admin DashBord</h1>
       </div>
-
-      {/* */}
     </div>
   );
 };
