700501b81d1fabafaa724668e285c179743857de
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-10-22 11:56:12 +0530

    add socket

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,22 @@ const purchaserouter = require("./src/Purchase/router");
 const cors = require("cors");
 const salesRouter = require("./src/Sales/router");
 const openStockRouter = require("./src/stock_inventroy/router");
+const UsersRouter = require("./src/Users/router");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["OPTIONS", "GET", "POST", "PUT", "DELETE"],
+  },
+});
+app.use((req, res, next) => {
+  req.io = io; // Attach io to req object
+  next();
+});
 app.use(express.json());
 
 app.use(
@@ -18,8 +33,43 @@ app.use(
 connection();
 app.use(router);
 app.use(purchaserouter);
-app.use(salesRouter);
+app.use(salesRouter(io));
 app.use(openStockRouter);
-app.listen(4000, () => {
+app.use(UsersRouter);
+
+app.post("/send", (req, res) => {
+  const message = req.body.message;
+  console.log(message);
+  index.js;
+  io.emit("message", { message });
+  res.status(200).send({
+    message: "Message sent successfully",
+  });
+
+  io.on("connection", (socket) => {
+    console.log("A client Connected");
+    socket.on("disconnect", () => {
+      console.log("Client disconnected");
+    });
+
+    // Emit an event named "TestEvent"
+    // setTimeout(() => {
+    // const alert = {
+    //   itemCode: "ABC123",
+    //   stockLevel: 5,
+    //   message: "Stock for ABC123 is low: 5 units left.",
+    // };
+    //   socket.emit("lowStockAlert", socket); // Emitting directly for testing
+    // }, 3000);
+
+    // socket.on("TestEvent", (data) => {
+    //   console.log("Recived from Client:", data);
+    // });
+  });
+});
+
+server.listen(4000, () => {
   console.log("Server Running on 4000.");
 });
+
+module.exports = io;
