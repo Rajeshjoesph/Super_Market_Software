7b563ade448c2539629af38cb9dbc6a91ea1f4c1
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-10-23 17:27:57 +0530

    Modifiy code role base route update

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import StorecontextProvider from "./Context/StoreContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StorecontextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StorecontextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
