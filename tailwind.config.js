154a2d60e7e197bac65cc9cea5b9118d28fc1b49
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-10-27 13:57:47 +0530

    code modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { colors } = require("@mui/material");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-red": "#f87171",
+      },
+    },
   },
   plugins: [],
 };
