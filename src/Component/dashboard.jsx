ea09e3ffd54d8494f0e9a7bc7e6f2165d541653a
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-10-27 14:09:19 +0530

    dashbord  module

--- src/Component/dashboard.jsx
+++ src/Component/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 // import Navbar from "./navnar";
 import Button from "./button";
 import Allproducts from "../product/Allproducts";
@@ -7,13 +7,20 @@ import { Link, useNavigate } from "react-router-dom";
 import Navbar from "./navnar";
 import Dashbord from "../Admin/AdminItem";
 import Notification from "./Notification";
+import { storeContext } from "../Context/StoreContext";
+import { capitalize } from "@mui/material";
 const Dashboard = () => {
+  const { users } = useContext(storeContext);
+
+  const capitalize =
+    users[0].role.charAt(0).toUpperCase() + users[0].role.slice(1);
+
   const navigate = useNavigate();
   return (
     <div className="flex">
       <Navbar />
       <div className="App  items-center  justify-between h-screen w-full space-y-4 ">
-        <h1 className="text-xl m-3">Admin DashBord</h1>
+        <h1 className="text-xl m-3">{capitalize} DashBord</h1>
         <Dashbord />
         <Notification />
       </div>
