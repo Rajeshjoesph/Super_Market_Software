cbfc55623dfd4a6638c6e92212fa87c547ff8203
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-10-27 14:03:30 +0530

    Sigin page module

--- src/Users/SiginPage.jsx
+++ src/Users/SiginPage.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { storeContext } from "../Context/StoreContext";
+import { Player } from "lottie-react";
+import loginAnimation from "../Pubilc/gifs/Tabletlogin.gif";
 
 const SiginPage = () => {
   const initstate = {
@@ -8,6 +11,7 @@ const SiginPage = () => {
     password: "",
   };
   const [sigin, setSigin] = useState(initstate);
+  const { setUsers } = useContext(storeContext);
   const navigate = useNavigate();
 
   const [errorMessage, setErrorMessage] = useState("");
@@ -17,14 +21,22 @@ const SiginPage = () => {
   };
   console.log(sigin);
 
+  const signOut = () => {
+    localStorage.removeItem("user"); // Clear user data on sign-out
+    setUsers([]); // Clear context state
+  };
+
   const handleSignIn = async (e) => {
     e.preventDefault();
     try {
       const SiginApi = await axios.post("http://localhost:4000/sigin", sigin);
       if (SiginApi.status === 200) {
+        const userData = SiginApi.data.data;
+        setUsers([userData]);
+        localStorage.setItem("user", JSON.stringify(userData));
         console.log("Sign in successful");
         setErrorMessage("");
-        navigate("/");
+        navigate("/dashbord");
       } else {
         setErrorMessage("Invalid email or password");
       }
@@ -34,70 +46,84 @@ const SiginPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold text-center mb-6">Sign In</h2>
+    <div className="min-h-screen flex flex-row-reverse justify-around items-center ">
+      <div className="h-screen w-2/5 flex-col justify-items-end py-6">
+        <div className="h-[95%] w-full max-w-md flex items-center">
+          {errorMessage && (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded mb-4">
+              {errorMessage}
+            </div>
+          )}
 
-        {errorMessage && (
-          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded mb-4">
-            {errorMessage}
-          </div>
-        )}
-
-        <form onSubmit={handleSignIn}>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              // value={sigin.email}
-              onChange={(e) => handleChange(e)}
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-indigo-500"
-              placeholder="Enter your email"
-              required
-            />
-          </div>
-
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              name="password"
-              // value={sigin.password}
-              onChange={(e) => handleChange(e)}
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-indigo-500"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
+          <form
+            className="w-5/6 flex flex-col text-start "
+            onSubmit={handleSignIn}
+          >
+            <h2 className="text-2xl text-theme-red font-bold text-start mb-6">
               Sign In
-            </button>
-            <a
-              href="#"
-              className="text-sm text-indigo-500 hover:text-indigo-600"
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
+            </h2>
+            <div className="mb-6">
+              {/* <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label> */}
+              <input
+                id="email"
+                type="email"
+                name="email"
+                // value={sigin.email}
+                onChange={(e) => handleChange(e)}
+                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-indigo-500 drop-shadow-lg"
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              {/* <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label> */}
+              <input
+                id="password"
+                type="password"
+                name="password"
+                // value={sigin.password}
+                onChange={(e) => handleChange(e)}
+                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-indigo-500 drop-shadow-lg"
+                placeholder="Enter your password "
+                required
+              />
+            </div>
+
+            <div className="flex flex-col-reverse w-40 items-start mt-8">
+              <button
+                type="submit"
+                className="bg-theme-red w-full  hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline my-4 drop-shadow-lg"
+              >
+                Sign In
+              </button>
+              <a
+                href="#"
+                className="text-sm text-indigo-500 hover:text-indigo-600"
+              >
+                Forgot Password?
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="flex justify-evenly">
+        <img
+          src={loginAnimation}
+          alt="Login Animation"
+          className="hidden md:block h-90 w-90"
+        />
       </div>
     </div>
   );
