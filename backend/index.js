824260da7f0b2e3d2902fad7fb4f256871a13eb9
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-10-28 16:47:59 +0530

    code modified

--- backend/index.js
+++ backend/index.js
@@ -8,21 +8,21 @@ const salesRouter = require("./src/Sales/router");
 const openStockRouter = require("./src/stock_inventroy/router");
 const UsersRouter = require("./src/Users/router");
 const http = require("http");
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 // const notifiyRouter = require("./src/Notification/router");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["OPTIONS", "GET", "POST", "PUT", "DELETE"],
-  },
-});
-app.use((req, res, next) => {
-  req.io = io; // Attach io to req object
-  next();
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["OPTIONS", "GET", "POST", "PUT", "DELETE"],
+//   },
+// });
+// app.use((req, res, next) => {
+//   req.io = io; // Attach io to req object
+//   next();
+// });
 app.use(express.json());
 
 app.use(
@@ -32,6 +32,9 @@ app.use(
   })
 );
 connection();
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
 app.use(router);
 app.use(purchaserouter);
 app.use(salesRouter(io));
