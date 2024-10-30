7add258b0d0a9740ec31436b51f12b2a03a33f89
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-10-30 16:43:32 +0530

    connect file added

---
+++ backend/src/config/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotEnv = require("dotenv").config();
+const { DB_HOST, DB_USER, DB_PASS } = process.env;
+
+const connection = () =>
+  mongoose
+    .connect(DB_HOST)
+    .then(() => {
+      console.log("Database connection...");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+module.exports = connection;
