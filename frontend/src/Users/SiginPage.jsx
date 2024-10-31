e98bee14b904dabe3fa6cc9088e4d36884a24df7
Rajeshjoesph <rajeshkannan1999.rk@gmail.com>
2024-10-31 22:28:17 +0530

    code modified 31-10:26p

--- frontend/src/Users/SiginPage.jsx
+++ frontend/src/Users/SiginPage.jsx
@@ -30,7 +30,7 @@ const SiginPage = () => {
     e.preventDefault();
     try {
       const SiginApi = await axios.post(
-        "https://super-market-software-git-main-rajeshjoesphs-projects.vercel.app/sigin",
+        "www.super-market-software.app/sigin",
         sigin
       );
       if (SiginApi.status === 200) {
